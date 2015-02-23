fa300dd9c8c0da6c8bc62043dfb885edb8ebb1f8
Sukky1103 <a.amnos.scene@gmail.com>
2015-02-24 01:24:20 +0900

    fix0223
    variable name changed

--- src/com/example/hackmanexe/Dreambit.java
+++ src/com/example/hackmanexe/Dreambit.java
@@ -7,23 +7,33 @@ public class Dreambit extends Enemy {
 	
 	private static final int HP = 200;
 	private Player player;
+	private Timer t;
 
-	public Dreambit(FrameInfo f, Player _player) {
+	public Dreambit(PanelInfo f, Player _player) {
 		super(f, HP);
 		this.player = _player;
 		
-		Timer t = new Timer();
+		t = new Timer();
 		t.scheduleAtFixedRate(new TimerTask() {
 			@Override
 			public void run() {
-				int playerLine = player.getCurrentFrameInfo().getLine(); // scope error can't fixed...
-				int ownLine = getCurrentFrameInfo().getLine();
+				int playerLine = player.getCurrentPanelInfo().getLine(); // scope error can't fixed...
+				int ownLine = getCurrentPanelInfo().getLine();
 				if(playerLine < ownLine) //自身より上にプレイヤーいたら
 					moveUp();
 				else if(playerLine > ownLine) //下にいたら
 					moveDown();
 			}
 		}, 0, 300);
+	}
+
+	@Override
+	void deathProcess() {
+		// TODO 自動生成されたメソッド・スタブ
+		if (t != null) {
+			t.cancel();
+			t = null;
+		}
 	}
 
 }
